e574174785a58ac0e75dbb64e548584e7bae8c1d
bradyshehu <shehu.brady@gmail.com>
2025-05-15 18:28:46 +0200

    add modify logic

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,11 +1,16 @@
 const posts = require("../data/posts");
 const postsArray = posts.posts;
+
+// INDEX
+
 const index = (req, res) => {
   res.json({
     description: "Lista di tutti i post",
     data: posts,
   });
 };
+
+// SHOW
 
 const show = (req, res) => {
   const id = parseInt(req.params.id);
@@ -26,6 +31,8 @@ const show = (req, res) => {
     data: post,
   });
 };
+
+// STORE
 
 const store = (req, res) => {
   console.log(req.body);
@@ -48,12 +55,14 @@ const store = (req, res) => {
   res.status(201).json(newPost);
 };
 
+// UPDATE
+
 const update = (req, res) => {
   const id = parseInt(req.params.id);
-  const post = postsArray.find((post) => post.id == id);
+  const oldPost = postsArray.find((post) => post.id == id);
   const { title, content, image, tags } = req.body;
 
-  if (!post) {
+  if (!oldPost) {
     res.status(404);
     res.json({
       error: "404 Not Found",
@@ -64,17 +73,19 @@ const update = (req, res) => {
   // AGGIUNGI CONTROLLO SU DATI INSERITI
 
   // LOGICA SOSTITUZIONE DI UN POST
-  const updatedPost = { id: post.id, title, content, image, tags };
-  const postIndex = postsArray.indexOf(post);
-  postsArray.splice(postIndex, 1, updatedPost);
+  const updatedPost = { id: oldPost.id, title, content, image, tags };
+  const oldPostIndex = postsArray.indexOf(oldPost);
+  postsArray.splice(oldPostIndex, 1, updatedPost);
   res.json(updatedPost);
 };
 
+// MODIFY
+
 const modify = (req, res) => {
   const id = parseInt(req.params.id);
-  const post = postsArray.find((post) => post.id == id);
+  const oldPost = postsArray.find((post) => post.id == id);
 
-  if (!post) {
+  if (!oldPost) {
     res.status(404);
     res.json({
       error: "404 Not Found",
@@ -83,8 +94,26 @@ const modify = (req, res) => {
     return;
   }
 
-  res.send("Modifica del post " + id);
+  const { title, content, image, tags } = req.body;
+
+  if (title) {
+    oldPost.title = title;
+  }
+
+  if (content) {
+    oldPost.content = content;
+  }
+  if (image) {
+    oldPost.image = image;
+  }
+  if (tags) {
+    oldPost.tags = tags;
+  }
+
+  res.json(oldPost);
 };
+
+// DESTROY
 
 const destroy = (req, res) => {
   const id = parseInt(req.params.id);
